fdae952aa92402fc7a51a23cd38d49c92d4553ed
TimeB <timairisov@gmail.com>
2026-03-01 18:01:30 +0300

    trying to make plugin

--- analyzer/analyzer.go
+++ analyzer/analyzer.go
@@ -3,7 +3,7 @@ package analyzer
 import "golang.org/x/tools/go/analysis"
 
 var Analyzer = &analysis.Analyzer{
-	Name: "myloglinter",
+	Name: "myloglint",
 	Doc: "Checks logs, logs must: " +
 		"start with the small case, " +
 		"to be only alphabet symbols and  in English, " +
---
+++ lint-plugin/go.mod
@@ -0,0 +1,8 @@
+module github.com/timeb30/loglint/lint-plugin
+
+go 1.25
+
+require (
+	github.com/timeb30/loglint v0.0.0-20260301144745-77e2a1a3debf // indirect
+	golang.org/x/tools v0.42.0 // indirect
+)
